0f03d4451516d7999a5add07c0c8c975b44f401e
Esteban Carter <carterea1999@gmail.com>
2021-06-11 14:43:22 -0400

    Final Product

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ImageForm extends Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      caption: '',
+      url: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.addImage(this.state);
+    this.setState({
+      caption: '',
+      url: '',
+    });
+  }
+
+  handleInput(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="caption" value={this.state.caption} onChange={this.handleInput}/>
+        <input type="url" name="url" value={this.state.url} onChange={this.handleInput}/>
+        <button type="submit">Save</button>
+      </form>
+    )
+  }
+};
+
+// class ImageList extends Component{
+//
+//   render() {
+//     const images = this.props.images.map((image, index) => (
+//       <li key={index}>
+//         <img src={image.url} alt=""/>
+//         <p>{image.caption}</p>
+//       </li>
+//     ));
+//
+//     return(
+//       <ul>
+//         {images}
+//       </ul>
+//     )
+//   }
+// };
+
+function ImageList(props) {
+    const images = props.images.map((image, index) => (
+      <li key={index}>
+        <img src={image.url} alt=""/>
+        <p>{image.caption}</p>
+      </li>
+    ));
+
+    return(
+      <ul>
+        {images}
+      </ul>
+    );
 }
 
-export default App;
+class ImageBoard extends Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: [],
+    }
+
+    this.addImage = this.addImage.bind(this);
+  }
+
+  componentDidMount() {
+    const images = [
+      {
+        caption: 'random image',
+        url: 'https://images.ctfassets.net/yr4qj72ki4ky/legacyBlogPost77Thumbnail/cd4783ad7b35efc4367166a570a9952e/bigstock-Real-Java-Script-Code-Developi-217215433.jpg'
+      }
+    ];
+
+    this.setState({ images });
+  }
+
+  addImage(image) {
+    const images = [...this.state.images];
+    images.push(image);
+    this.setState({images});
+  }
+
+  render() {
+    console.log(this)
+    return(
+      <div>
+        <ImageForm addImage={this.addImage}/>
+        <ImageList images={this.state.images}/>
+      </div>
+    )
+  }
+}
+
+export default ImageBoard;
